bdadbbd9c00e2c18c001ac6f572814d907ea8d02
tay2345-gif <tashuanlawrence7@gmail.com>
2025-06-28 20:03:33 -0400

    Web Application

--- App/app.py
+++ App/app.py
@@ -1,21 +1,37 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request
 from game_logic import play_game
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "🕵️ Welcome to 007 Island (DevOps Edition). Use /play endpoint."
+@app.route('/', methods=['GET', 'POST'])
+def game():
+    message = "Your mission is to find the treasure!"
+    step = 1
+    choices = {}
 
-@app.route('/play', methods=['POST'])
-def play():
-    data = request.get_json()
-    choice1 = data.get('choice1')
-    choice2 = data.get('choice2')
-    choice3 = data.get('choice3')
+    if request.method == 'POST':
+        choice1 = request.form.get('choice1')
+        choice2 = request.form.get('choice2')
+        choice3 = request.form.get('choice3')
 
-    result = play_game(choice1, choice2, choice3)
-    return jsonify({'result': result})
+        choices['choice1'] = choice1
+        choices['choice2'] = choice2
+        choices['choice3'] = choice3
+
+        # Determine which step we're on
+        if choice1 and not choice2:
+            message = play_game(choice1)
+            step = 2
+        elif choice1 and choice2 and not choice3:
+            message = play_game(choice1, choice2)
+            step = 3
+        elif choice1 and choice2 and choice3:
+            message = play_game(choice1, choice2, choice3)
+            step = 1  # Reset for new game
+            choices = {}
+
+    return render_template('index.html', message=message, step=step, choices=choices)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
+
